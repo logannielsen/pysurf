d41e1652bff683c85348b2c80f0c3941117d26a7
Logan <logannielsen@live.com>
2019-08-05 15:43:57 +1000

    performs get request and returns htm as text

--- pysurf/helpers.py
+++ pysurf/helpers.py
@@ -5,17 +5,20 @@ requests.headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit"
     "/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
+url = "https://httpbin.org/get"
 
 
 def http_get(url):
     """Receives a url and performs a GET request,
     returns the html dom as text
     """
-
-    pass
+    request = requests.get(url).text
+    return request
 
 
 # writing the data to a file
+def write_request_to_file():
+    """write request to file"""
 
 
 # determine if a file exists and return it or return None
